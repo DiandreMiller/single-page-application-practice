f761f4eba20c930d3646446a88a6a1e12fdabec1
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2024-03-12 01:11:35 -0400

    chart added

--- src/HelloFresh/HelloFreshChart.tsx
+++ src/HelloFresh/HelloFreshChart.tsx
@@ -1,17 +1,13 @@
 import './HelloFreshChart.css'
-import HelloFreshLogo from '../assets/image/hello-fresh-logo.png'
+import HelloFreshChartImage from '../assets/image/hello-fresh-chart.png'
+
 
 const HelloFreshChart = () => {
     return (
         <div>
             <section className='chart-container'>
                 <h2 className='lifestyle'>Built For Your Lifestyle</h2>
-                <table className='actual-chart'>
-                <img className='hello-fresh-logo1' src={HelloFreshLogo} alt="hello fresh logo" />
-                <img className='shopping-cart' src='https://cdn.icon-icons.com/icons2/1369/PNG/512/-shopping-cart_90604.png' alt='shopping cart'/>
-                <p className='local-store'>YOUR LOCAL GROCERY STORE</p>
-
-                </table>
+                <img className='chart-image' src={HelloFreshChartImage} alt='chart'/>
                 
             </section>
         </div>
